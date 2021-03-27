2cf16dff3c73385931ca0151a03a81411b59796d
Connor Kerwin <connorkerwin@yahoo.co.uk>
2021-03-27 22:03:22 +0000

    Chicken AI!

--- Assets/Scripts/AI/ChickenAI.cs
+++ Assets/Scripts/AI/ChickenAI.cs
@@ -7,13 +7,55 @@ public class ChickenAI : MonoBehaviour
 {
     public NavMeshAgent Agent;
 
+    public float DistanceThreshold = 0.1f;
+    public float PathRate = 1.0f;
+
     private Vector3 destinationGoal;
-    private float destinationRate;
     private float time;
     private bool hasPath;
 
     private void Update()
     {
         time += Time.deltaTime;
+
+        if (hasPath)
+        {
+            float dist = Vector3.Distance(transform.position, destinationGoal);
+            if(dist <= DistanceThreshold)
+            {
+                LayEgg();
+                hasPath = false;
+            }
+        }
+        else
+        {
+            if(time >= PathRate)
+            {
+                GenerateNewPath();
+                time = 0.0f;
+            }
+        }
+    }
+
+    private void LayEgg()
+    {
+        var inst = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        inst.transform.position = transform.position;
+    }
+
+    private void GenerateNewPath()
+    {
+        Vector3 destination = transform.position;
+
+        float rand = Random.Range(0.0f, 360.0f);
+        destination += PointOnCircle(rand, 5.0f);
+        destinationGoal = destination;
+
+        hasPath = Agent.SetDestination(destination);
+    }
+
+    private Vector3 PointOnCircle(float angle, float distance)
+    {
+        return new Vector3(distance * Mathf.Sin(angle), 0.0f, distance * Mathf.Cos(angle));
     }
 }
