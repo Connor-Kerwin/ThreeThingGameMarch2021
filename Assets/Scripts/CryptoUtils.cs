0f1c5facb3386c184830842cb3b9350b09eadd7f
Connor Kerwin <connorkerwin@yahoo.co.uk>
2021-03-28 10:31:45 +0100

    Highscore behaviour added. Tested working

--- Assets/Scripts/CryptoUtils.cs
+++ Assets/Scripts/CryptoUtils.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public static class CryptoUtils
 {
-    public static string Md5Sum(string strToEncrypt)
-{
-	System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
-	byte[] bytes = ue.GetBytes(strToEncrypt);
- 
-	// encrypt bytes
-	System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
-	byte[] hashBytes = md5.ComputeHash(bytes);
- 
-	// Convert the encrypted bytes back to a string (base 16)
-	string hashString = "";
- 
-	for (int i = 0; i < hashBytes.Length; i++)
+	public static string Md5Sum(string strToEncrypt)
 	{
-		hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+		System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
+		byte[] bytes = ue.GetBytes(strToEncrypt);
+
+		// encrypt bytes
+		System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+		byte[] hashBytes = md5.ComputeHash(bytes);
+
+		// Convert the encrypted bytes back to a string (base 16)
+		string hashString = "";
+
+		for (int i = 0; i < hashBytes.Length; i++)
+		{
+			hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+		}
+
+		return hashString.PadLeft(32, '0');
 	}
- 
-	return hashString.PadLeft(32, '0');
-}
 }
--- Assets/Scripts/GameManagement/ScoreSystem.cs
+++ Assets/Scripts/GameManagement/ScoreSystem.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ScoreSystem : GameSystem<ScoreSystem>
 {
+    public string GameId;
+
     public int Score { get; private set; }
+
+    private string secret;
+
+    protected override void Awake()
+    {
+        secret = HighscoreSecret.Secret;
+
+        base.Awake();
+    }
 
     public void ResetScore()
     {
@@ -26,4 +39,43 @@ public class ScoreSystem : GameSystem<ScoreSystem>
     {
         await Task.Delay(1000);
     }
+
+
+
+
+    public void SubmitScore(int score, string name)
+    {
+        var hash = this.generateHash(score, name);
+        var json = $"{{\"Hash\":\"{hash}\",\"UserName\": \"{name}\", \"Score\": \"{score}\", \"GameId\":\"{this.GameId}\"}}";
+
+        StartCoroutine(POST(json, "https://chickensimulator.com/api/submit"));
+    }
+
+    public IEnumerator POST(string json, string url)
+    {
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        if (request.error != null)
+        {
+            Debug.Log("Erro: " + request.error);
+        }
+        else
+        {
+            Debug.Log("All OK");
+            Debug.Log("Status Code: " + request.responseCode);
+        }
+    }
+
+
+    private string generateHash(int score, string name)
+    {
+        var verifyString = this.secret + this.GameId + score + name;
+        return CryptoUtils.Md5Sum(verifyString);
+    }
+
 }
