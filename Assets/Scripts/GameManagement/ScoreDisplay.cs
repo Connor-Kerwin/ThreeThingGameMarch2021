15104ac77ef5d7e093835f1f4f53fc39efaeaef8
Connor Kerwin <connorkerwin@yahoo.co.uk>
2021-03-28 14:04:42 +0100

    Highscores work!

--- Assets/Scripts/GameManagement/ScoreDisplay.cs
+++ Assets/Scripts/GameManagement/ScoreDisplay.cs
@@ -1,6 +1,38 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class ScoreDisplay : MonoBehaviour
 {
-   // public
+    public ScoreEntry[] scores;
+
+    private void Start()
+    {
+        HighscoreSystem.Instance.OnScoresChanged += OnScoresChanged;
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        List<ScoreData> temp = new List<ScoreData>();
+        HighscoreSystem.Instance.GetScores(temp);
+
+        // Reset
+        for(int i = 0; i < scores.Length; i++)
+        {
+            ScoreEntry entry = scores[i];
+            entry.SetValues("---", 0);
+        }
+
+        int num = Mathf.Clamp(temp.Count, 0, scores.Length);
+        for(int i = 0; i < num; i++)
+        {
+            ScoreEntry entry = scores[i];
+            entry.SetValues(temp[i].UserName, temp[i].Score);
+        }
+    }
+
+    private void OnScoresChanged()
+    {
+        Refresh();
+    }
 }
