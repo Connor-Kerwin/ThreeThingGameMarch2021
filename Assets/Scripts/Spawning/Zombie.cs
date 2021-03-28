64f36119955ae33ff1d3ebb3df872d2c0b07093c
AlexLovett <alex.denys.lovett@gmail.com>
2021-03-28 16:11:54 +0100

    zombies get faster over time

--- Assets/Scripts/Spawning/Zombie.cs
+++ Assets/Scripts/Spawning/Zombie.cs
@@ -1,6 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
+    public float SpeedupScale;
+    private NavMeshAgent navMeshAgent;
+    private void Start()
+    {
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+    }
 
+    private void Update()
+    {
+        if (navMeshAgent)
+        {
+            navMeshAgent.speed += SpeedupScale * Time.deltaTime;
+        }
+    }
 }
